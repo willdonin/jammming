31b7492bd39445d57a79f3191f287a6bd0cfdd09
Will Donin <guilherme@donin.com.br>
2023-05-19 10:54:34 -0400

    Search box style updated

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -30,13 +30,13 @@ export default function SearchBar() {
         <input
           type="search"
           id="default-search"
-          className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="block w-full p-4 pl-10 text-sm ring border rounded-lg bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-purple-400/75 outline-0"
           placeholder="Search Songs, Artists..."
           required
         ></input>
         <button
           type="submit"
-          className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white absolute right-2.5 bottom-2.5 font-medium rounded-lg text-sm px-4 py-2 bg-purple-600 hover:bg-purple-700"
         >
           Search
         </button>
