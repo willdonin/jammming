fc7fcc223ed6d89ae7e2e0c1594c7130d9cffe8f
Will Donin <guilherme@donin.com.br>
2023-05-19 13:03:09 -0400

    Fix component miss use, Tracklist was implementes on SearchResult.

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Nav from "@/components/Nav";
 import SearchBar from "@/components/SearchBar";
-import SearchResults from "@/components/SearchResults";
+import Tracklist from "@/components/Tracklist";
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
         <SearchBar />
       </div>
       <div className="py-3 mx-auto max-w-md sm:max-w-lg md:max-w-2xl">
-        <SearchResults />
+        <Tracklist />
       </div>
     </>
   );
--- components/Tracklist.jsx
+++ components/Tracklist.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from "react";
+import Track from "./Track";
 
 export default function Tracklist() {
+  const songs = [
+    {
+      id: 1,
+      songName: "Let It Go",
+      artist: "Idina Menzel",
+      album: "Frozen",
+    },
+    {
+      id: 2,
+      songName: "Fear of the dark",
+      artist: "iron maiden",
+      album: "fear of the dark",
+    },
+    {
+      id: 3,
+      songName: "I can't wait forever",
+      artist: "simple plan",
+      album: "perfect",
+    },
+  ];
+
   return (
-    <div>Tracklist</div>
-  )
+    <div className="relative bg-purple-600/[0.40] rounded-lg p-4">
+      <div className="flex items center">
+        <ul className="w-full">
+          {songs.length !== 0 ? (
+            songs.map((song) => {
+              return (
+                <Track
+                  id={song.id}
+                  name={song.songName}
+                  artist={song.artist}
+                  album={song.album}
+                />
+              );
+            })
+          ) : (
+            <li className="text-white text-center">0 results found.</li>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 }
