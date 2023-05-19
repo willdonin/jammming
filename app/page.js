83ce6ad0968b79b9aa584314672a1b2e6ef3637c
Will Donin <guilherme@donin.com.br>
2023-05-19 00:52:00 -0400

    Search bar design added.

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Nav from "@/components/Nav";
+import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
   return (
     <>
       <Nav />
+      <div className="py-10 mx-auto max-w-md">
+        <SearchBar />
+      </div>
     </>
   );
 }
